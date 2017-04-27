25236cbf28e4a015718d8888888f6bc8a9bf8cf8
chvsai <chadaramlakshmi@gmail.com>
2017-04-27 16:49:52 +0530

    Create FCITregistrations.java

---
+++ FCITregistrations.java
@@ -0,0 +1,13 @@
+
+public class FCITregistrations {
+	private FCITstudent head;
+	private int noOfRegistrations;
+	private FCITstudent next;
+	public boolean isEmpty( ){
+		return this.noOfRegistrations==0;
+	};
+	public void insert( FCITstudent head ){}; 
+	//private FCITstudent insert( parameters here ){}; 
+	public void PrintRegistrations( ){} ;
+	public void PrintRegistrations( FCITstudent head ){};
+}
