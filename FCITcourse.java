243d62a3b305b14fd3f4073184e04fcfd239c284
chvsai <chadaramlakshmi@gmail.com>
2017-04-27 16:48:02 +0530

    Create FCITcourse.java

---
+++ FCITcourse.java
@@ -0,0 +1,24 @@
+
+public class FCITcourse {
+	private String courseNumber; 
+	private String courseDays; 
+	private String courseTime;
+	public String getCourseNumber() {
+		return courseNumber;
+	}
+	public void setCourseNumber(String courseNumber) {
+		this.courseNumber = courseNumber;
+	}
+	public String getCourseDays() {
+		return courseDays;
+	}
+	public void setCourseDays(String courseDays) {
+		this.courseDays = courseDays;
+	}
+	public String getCourseTime() {
+		return courseTime;
+	}
+	public void setCourseTime(String courseTime) {
+		this.courseTime = courseTime;
+	}
+}
